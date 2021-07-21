fee1563d7b6d4144fb06b90df4d360ad2af285ea
Layl Bongers <2385329-layl@users.noreply.gitlab.com>
2021-07-21 06:23:15 +0200

    Fix wrapper generation

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,9 +1,15 @@
+use machinery::foundation::{ApiRegistryApi, LoggerApi};
 use machinery_sys::foundation::{tm_api_registry_api, tm_logger_api, TM_LOG_TYPE_INFO};
 
 #[no_mangle]
-pub unsafe extern "C" fn tm_load_plugin(reg: *const tm_api_registry_api, load: bool) {
-    let logger = (*reg).get(tm_logger_api::NAME) as *const tm_logger_api;
+pub unsafe extern "C" fn tm_load_plugin(registry: *const tm_api_registry_api, load: bool) {
+    let registry = ApiRegistryApi(registry);
 
-    let text = format!("Example rust plugin {}.", if load { "loaded" } else { "unloaded" });
-    (*logger).printf(TM_LOG_TYPE_INFO, &text);
+    let logger = LoggerApi(registry.get(LoggerApi::NAME) as *const tm_logger_api);
+
+    let text = format!(
+        "Example rust plugin {}.",
+        if load { "loaded" } else { "unloaded" }
+    );
+    logger.printf(TM_LOG_TYPE_INFO, &text);
 }
--- machinery/src/generated/mod.rs
+++ machinery/src/generated/mod.rs
@@ -1,2 +1,1 @@
 pub mod foundation;
-mod generated;
