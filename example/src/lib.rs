1e10f6fff5cdfb809cd3ff71f597dd5b49e6b0c4
Layl Bongers <2385329-layl@users.noreply.gitlab.com>
2021-07-21 16:34:27 +0200

    Improve plugin API to work well with machinery

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,9 +1,4 @@
-use std::{
-    ffi::c_void,
-    mem::ManuallyDrop,
-    os::raw::c_char,
-    sync::{RwLock, RwLockReadGuard, RwLockWriteGuard},
-};
+use std::{ffi::c_void, os::raw::c_char};
 
 use const_cstr::const_cstr;
 use machinery::{
@@ -19,76 +14,62 @@ use machinery_sys::{
     },
     plugins::the_machinery_shared::tm_ci_editor_ui_i,
 };
-use once_cell::sync::OnceCell;
 use tracing::{event, Level};
 
 plugin!(ExamplePlugin);
 
-enum ExamplePlugin {}
+struct ExamplePlugin {
+    registry: ApiRegistryApi,
+    truth_api: TheTruthApi,
+    truth_common_types: TheTruthCommonTypesApi,
+    editor_aspects: Vec<Box<tm_ci_editor_ui_i>>,
+}
 
 impl Plugin for ExamplePlugin {
-    fn load(registry: &ApiRegistryApi) {
-        let state = PluginState {
+    fn load(registry: ApiRegistryApi) -> Self {
+        let plugin = ExamplePlugin {
             truth_api: registry.get(),
             truth_common_types: registry.get(),
             editor_aspects: Vec::new(),
+            registry,
         };
-        STATE
-            .set(RwLock::new(ManuallyDrop::new(state)))
-            .unwrap_or_else(|_| panic!("STATE was already set"));
 
         unsafe {
-            registry.add_implementation(
+            plugin.registry.add_implementation(
                 TM_THE_TRUTH_CREATE_TYPES_INTERFACE_NAME,
                 truth_create_types as *const c_void,
             );
         }
 
         event!(Level::INFO, "Example rust plugin loaded.");
-    }
 
-    fn unload(registry: &ApiRegistryApi) {
+        plugin
+    }
+}
+
+impl Drop for ExamplePlugin {
+    fn drop(&mut self) {
         unsafe {
-            registry.remove_implementation(
+            self.registry.remove_implementation(
                 TM_THE_TRUTH_CREATE_TYPES_INTERFACE_NAME,
                 truth_create_types as *const c_void,
             );
-        }
-
-        unsafe {
-            let mut state = STATE.get().unwrap().write().unwrap();
-            ManuallyDrop::drop(&mut state);
         }
 
         event!(Level::INFO, "Example rust plugin unloaded.");
     }
 }
 
-static STATE: OnceCell<RwLock<ManuallyDrop<PluginState>>> = OnceCell::new();
-
-#[allow(dead_code)]
-fn read_state() -> RwLockReadGuard<'static, ManuallyDrop<PluginState>> {
-    STATE.get().unwrap().read().unwrap()
-}
-
-fn write_state() -> RwLockWriteGuard<'static, ManuallyDrop<PluginState>> {
-    STATE.get().unwrap().write().unwrap()
-}
-
-struct PluginState {
-    truth_api: TheTruthApi,
-    truth_common_types: TheTruthCommonTypesApi,
-    editor_aspects: Vec<Box<tm_ci_editor_ui_i>>,
-}
-
 extern "C" fn truth_create_types(tt: *mut tm_the_truth_o) {
     event!(Level::INFO, "Registering truth types");
 
-    let mut state = write_state();
+    let mut guard = ExamplePlugin::write();
+    let plugin = guard.as_mut().unwrap();
 
     unsafe {
-        state.truth_common_types.create_common_types(tt);
+        plugin.truth_common_types.create_common_types(tt);
 
+        // Create a the example component
         let properties = tm_the_truth_property_definition_t {
             name: const_cstr!("angular_velocity").as_ptr(),
             type_: TM_THE_TRUTH_PROPERTY_TYPE_SUBOBJECT as u32,
@@ -96,27 +77,28 @@ extern "C" fn truth_create_types(tt: *mut tm_the_truth_o) {
             ..Default::default()
         };
 
-        let spin_type = (*state.truth_api.0).create_object_type.unwrap()(
+        let spin_type = (*plugin.truth_api.0).create_object_type.unwrap()(
             tt,
             const_cstr!("tm_rust_example_component").as_ptr(),
             &properties,
             1,
         );
-        state
+        plugin
             .truth_api
             .set_default_object_to_create_subobjects(tt, spin_type);
 
+        // Register the component with the editor
         let editor_aspect = Box::new(tm_ci_editor_ui_i {
             category: Some(component_category),
             ..Default::default()
         });
-        state.truth_api.set_aspect(
+        plugin.truth_api.set_aspect(
             tt,
             spin_type,
             TM_CI_EDITOR_UI,
             &*editor_aspect as *const _ as *const _,
         );
-        state.editor_aspects.push(editor_aspect);
+        plugin.editor_aspects.push(editor_aspect);
     }
 }
 
--- machinery/src/lib.rs
+++ machinery/src/lib.rs
@@ -3,43 +3,72 @@ mod generated;
 pub mod plugins;
 mod tracing_sub;
 
+use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+
 use foundation::ApiRegistryApi;
 use machinery_sys::foundation::tm_api_registry_api;
+use once_cell::sync::OnceCell;
 
 use crate::tracing_sub::MachinerySubscriber;
+
+pub type PluginInstance<P> = OnceCell<RwLock<Option<P>>>;
+
+pub type PluginReadGuard<P> = RwLockReadGuard<'static, Option<P>>;
+
+pub type PluginWriteGuard<P> = RwLockWriteGuard<'static, Option<P>>;
 
 #[macro_export]
 macro_rules! plugin {
     ($ty:ident) => {
+        static INSTANCE: machinery::PluginInstance<$ty> = machinery::PluginInstance::new();
+
         #[no_mangle]
         pub unsafe extern "C" fn tm_load_plugin(
             registry: *const machinery_sys::foundation::tm_api_registry_api,
             load: bool,
         ) {
-            machinery::load_plugin::<$ty>(registry, load);
+            machinery::load_plugin::<$ty>(&INSTANCE, registry, load);
+        }
+
+        impl $ty {
+            fn read() -> machinery::PluginReadGuard<Self> {
+                INSTANCE.get().unwrap().read().unwrap()
+            }
+
+            fn write() -> machinery::PluginWriteGuard<Self> {
+                INSTANCE.get().unwrap().write().unwrap()
+            }
         }
     };
 }
 
-pub fn load_plugin<P: Plugin>(registry: *const tm_api_registry_api, load: bool) {
-    let registry = ApiRegistryApi(registry);
-
+pub fn load_plugin<P: Plugin>(
+    instance: &PluginInstance<P>,
+    registry: *const tm_api_registry_api,
+    load: bool,
+) {
     if load {
+        let registry = ApiRegistryApi(registry);
+
         // Initialize logging
         let subscriber = MachinerySubscriber::new(&registry);
         tracing::subscriber::set_global_default(subscriber).unwrap();
 
         // Load the plugin
-        P::load(&registry);
+        let plugin = P::load(registry);
+        let result = instance.set(RwLock::new(Some(plugin)));
+
+        if result.is_err() {
+            panic!("Plugin was already loaded!");
+        }
     } else {
-        // Unload the plugin
-        P::unload(&registry);
+        // Unload the plugin by dropping it
+        instance.get().unwrap().write().unwrap().take();
     }
 }
 
-pub trait Plugin {
-    fn load(registry: &ApiRegistryApi);
-    fn unload(registry: &ApiRegistryApi);
+pub trait Plugin: Sized {
+    fn load(registry: ApiRegistryApi) -> Self;
 }
 
 pub trait Api {
